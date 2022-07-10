2598a44bc543c52ea7179906192ecd25579b419c
jyf-111 <1920950476@qq.com>
2022-07-10 21:24:50 +0800

    .

--- Python_from_in_to_practice/django/learning_logs/views.py
+++ Python_from_in_to_practice/django/learning_logs/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import redirect, render
 
-from learning_logs.forms import TopicForm
-
 from .models import Topic
-
-# Create your views here.
-
+from learning_logs.forms import EntryForm, TopicForm
 
 def index(request):
     return render(request, 'learning_logs/index.html')
@@ -21,7 +17,7 @@ def topics(request):
 def topic(request, topic_id):
     topics = Topic.objects.get(id=topic_id)
     entries = topics.entry_set.order_by('date_added')
-    context = {'id': topic_id, 'topics': topics, 'entries': entries}
+    context = {'id': topic_id, 'topic': topics, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
 
@@ -35,3 +31,18 @@ def new_topic(request):
             return redirect('learning_logs:topics')
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
