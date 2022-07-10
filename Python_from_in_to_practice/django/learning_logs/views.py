061341c236febf089dbbea8ebf729f697b1f7001
jyf-111 <1920950476@qq.com>
2022-07-10 14:58:29 +0800

    .

--- Python_from_in_to_practice/django/learning_logs/views.py
+++ Python_from_in_to_practice/django/learning_logs/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from learning_logs.forms import TopicForm
 
 from .models import Topic
 
@@ -19,5 +21,17 @@ def topics(request):
 def topic(request, topic_id):
     topics = Topic.objects.get(id=topic_id)
     entries = topics.entry_set.order_by('date_added')
-    context = {'topics': topics, 'entries': entries}
+    context = {'id': topic_id, 'topics': topics, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
